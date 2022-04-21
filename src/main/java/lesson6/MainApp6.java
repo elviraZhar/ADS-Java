c9add3d183590ef54079eaaa923a4489c9c1bf48
ElviraZharenkova <elvira_bitl@list.ru>
2022-04-21 21:11:08 +0800

    lesson 6

--- src/main/java/lesson6/MainApp6.java
+++ src/main/java/lesson6/MainApp6.java
@@ -1,4 +1,17 @@
 package lesson6;
 
+import java.util.Random;
+
 public class MainApp6 {
-}
+
+    public static void main (String[] args) {
+        Random random = new Random();
+
+        TreeImpl<Integer> test = new TreeImpl<>();
+        while (test.size() < 14) {
+            test.add(random.nextInt(50) - 25);
+        }
+        test.display();
+    }
+}// Из 20 запусков 1 (5%) дерево можно назвать "сбалансированным" - была поглешность на 1 элемент ушедший в 5 уровень
+// остальные 19 были несбалансированы
